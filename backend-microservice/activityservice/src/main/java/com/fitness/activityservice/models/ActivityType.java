38206ada3df178728aaaecb0eee93cbe9295ab91
jaarvis007 <aj637061@gmail.com>
2025-12-29 15:46:03 +0530

    add activity model and activity type enum

---
+++ backend-microservice/activityservice/src/main/java/com/fitness/activityservice/models/ActivityType.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.models;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    PILATES,
+    YOGA,
+    Zumba,
+    SPINNING,
+    HIIT,
+    CROSSFIT,
+    OTHER
+}
