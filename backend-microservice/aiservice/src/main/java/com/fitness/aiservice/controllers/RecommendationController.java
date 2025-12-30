e5925606c2255acbc53e3d4f46b5a39d014573ac
jaarvis007 <aj637061@gmail.com>
2025-12-30 15:27:23 +0530

    feat: initialize Eureka server with Maven wrapper and dependencies

---
+++ backend-microservice/aiservice/src/main/java/com/fitness/aiservice/controllers/RecommendationController.java
@@ -0,0 +1,12 @@
+package com.fitness.aiservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommendation")
+public class RecommendationController {
+}
